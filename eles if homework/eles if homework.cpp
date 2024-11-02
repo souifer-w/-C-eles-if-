6d41eaa2ce3c1f92da71ba616f43ac7e86de6430
HP <HP@DESKTOP-NMBILJD>
2024-11-02 20:07:47 +0100

    Add project files.

---
+++ eles if homework/eles if homework.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int age = 21;
+
+	if (age < 20)
+	{
+
+		cout << "he's accept to new work \n";
+
+
+	}
+
+	else if (age < 17)
+	{
+		cout << " he's not accept to new work \n ";
+
+	}
+
+	else
+	{
+		cout << "he's accept \n";
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
